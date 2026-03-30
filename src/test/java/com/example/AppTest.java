7a74700876beaadf141634437ffc4aefb1359750
chinnupsna <119423293+chinnupsna@users.noreply.github.com>
2026-03-30 02:50:12 -0700

    secret

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest {
+public class AppTest { 
 
     @Test
     void testAdd() {
